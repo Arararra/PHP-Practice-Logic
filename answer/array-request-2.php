16fded70f61772f692e610b92cadf33b7d2462b9
Rama <mramamaulana60@gmail.com>
2019-10-08 09:25:57 +0700

    update array-request-2

--- answer/array-request-2.php
+++ answer/array-request-2.php
@@ -5,6 +5,11 @@
     ['id' => 423, 'nama' => 'Fakhri', 'asal' => 'Surabaya'],
   ];
 
+  $cari = '';
+  if (isset($_GET['cari'])) {
+    $cari = $_GET['cari'];
+  }
+
   if (isset($_GET['id'])){
     $id = $_GET['id'];
   }else{
@@ -18,51 +23,62 @@
   }
 
   if (isset($res)):
-    echo "Profil siswa id: ".$input[$res]['id']."<br>";
-    echo "- Id: ".$input[$res]['id']."<br>";
-    echo "- Nama: ".$input[$res]['nama']."<br>";
-    echo "- Asal: ".$input[$res]['asal']."<br>";
 ?>
 
+<p><?php echo "Menampilkan profil siswa id: ".$input[$res]['id'] ?></p>
+<p>
+  - ID: <?php echo $input[$res]['id'] ?><br>
+  - Nama: <?php echo $input[$res]['nama'] ?><br>
+  - Asal: <?php echo $input[$res]['asal'] ?><br>
+</p>
 <button onclick="window.history.back()">Kembali</button>
 
 <?php
   else:
 ?>
 
-<form>
-  <input type="search" name="search" placeholder="Search Id">
-  <input type="submit" value="Cari">
+<form action="#" method="get">
+  <input type="number" name="cari" placeholder="cari">
+  <input type="submit" value="filter">
+  <input type="reset" value="reset">
 </form>
 
-<?php
-  if (isset($_GET['search'])){
-    $search = $_GET['search'];
-  }else{
-    $search = "";
-  }
-  foreach ($input as $key => $value) {
-    $length = $value['id'];
-    $la = strlen($length);
-    echo $la;
-  }
+<table border="1" style="border-collapse: collapse;">
+  <tr>
+    <th>id</th>
+    <th>nama</th>
+    <th>asal</th>
+    <th>link</th>
+  </tr>
+
+<?php 
+  foreach ($input as $key => $value):
+  if ($value['id'] == $cari):
 ?>
 
-<table border="1" cellspacing="0">
-  <tr>
-    <td>Id</td>
-    <td>Nama</td>
-    <td>Asal</td>
-    <td>Link</td>
-  </tr>
-  <?php foreach ($input as $key => $value): ?>
   <tr>
     <td><?php echo $value['id'] ?></td>
     <td><?php echo $value['nama'] ?></td>
     <td><?php echo $value['asal'] ?></td>
     <td><a href="?id=<?php echo $value['id'] ?>">Profil</a></td>
   </tr>
-  <?php endforeach ?>
+
+<?php 
+  elseif ($cari == ''):
+?>
+
+  <tr>
+    <td><?php echo $value['id'] ?></td>
+    <td><?php echo $value['nama'] ?></td>
+    <td><?php echo $value['asal'] ?></td>
+    <td><a href="?id=<?php echo $value['id'] ?>">Profil</a></td>
+  </tr>
+
+<?php
+  endif; 
+  endforeach; 
+?>
+
 </table>
 
 <?php endif; ?>
